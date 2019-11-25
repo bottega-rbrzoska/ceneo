1520d7f3fa1ea05e55098d3d6db38aa9f2f7ed3d
Rafał Brzoska <rbrzoska@future-processing.com>
2019-11-25 13:31:47 +0100

    products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ProductAddComponent } from './product/product-add/product-add.component';
+import { ProductEditComponent } from './product/product-edit/product-edit.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductFormComponent } from './product/product-form/product-form.component';
+import { ProductsComponent } from './product/products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    ProductAddComponent,
+    ProductEditComponent,
+    ProductListComponent,
+    ProductFormComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
