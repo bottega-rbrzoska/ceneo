4f54b4ee0af02101258120c3ec45e12ad581bbcf
Rafał Brzoska <rbrzoska@future-processing.com>
2019-11-26 11:27:47 +0100

    navigation p2

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'ce-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  subscription: Subscription;
+  isLoggedIn: boolean;
+  constructor(private authService: AuthService) {
+   this.subscription =  authService.isLoggedIn$.subscribe( l => this.isLoggedIn = l );
+  }
 
   ngOnInit() {
   }
+
+  login() {
+    this.authService.logIn();
+  }
+
+  logout() {
+    this.authService.logOut();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
 
 }
