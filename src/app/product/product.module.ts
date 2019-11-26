5fa8e1511713f622a0f4a2eed03df5e0d61b97cf
Rafał Brzoska <rbrzoska@future-processing.com>
2019-11-26 14:08:50 +0100

    Lazy loaded module

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductFormComponent } from './product-form/product-form.component';
 
-
-
+const routes: Routes = [
+  { path: '', component: ProductsComponent },
+  { path: 'add', component: ProductAddComponent },
+  { path: 'edit/:id', component: ProductEditComponent }
+]
 @NgModule({
-  declarations: [],
+  declarations: [
+
+    ProductAddComponent,
+    ProductEditComponent,
+    ProductListComponent,
+    ProductFormComponent,
+    ProductsComponent,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class ProductModule { }
