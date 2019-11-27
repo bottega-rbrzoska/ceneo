2809f5fd341a266ab6aab4239bcc4e1e99f1150b
Rafał Brzoska <rbrzoska@future-processing.com>
2019-11-27 15:41:45 +0100

    removed tests

--- src/app/shared/directives/my-if.directive.spec.ts
+++ src/app/shared/directives/my-if.directive.spec.ts
@@ -1,8 +1,8 @@
-import { MyIfDirective } from './my-if.directive';
-
-describe('MyIfDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MyIfDirective();
-    expect(directive).toBeTruthy();
-  });
-});
+// import { MyIfDirective } from './my-if.directive';
+//
+// describe('MyIfDirective', () => {
+//   it('should create an instance', () => {
+//     const directive = new MyIfDirective();
+//     expect(directive).toBeTruthy();
+//   });
+// });
